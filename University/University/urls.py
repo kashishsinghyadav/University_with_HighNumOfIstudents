bbb4cbfd368aed7802f374986ce05e470bdeefb3
kashishsinghyadav <kashishhsinghhh@gmail.com>
2024-02-26 18:47:11 +0530

    yupp

--- University/University/urls.py
+++ University/University/urls.py
@@ -20,6 +20,6 @@ from TopUniversity.views import my_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('topstu/<str:firstcity>/<str:seccity>/', my_view, name='top_stu'),  # Added name argument
+    path('topstu/<str:firstcity>/<str:seccity>/', my_view, name='top_stu'),  
 ]
 
